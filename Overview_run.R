ebd3495b803129c653e5fc0924c0f8a9316b08ad
GwenKnight <gwen.knight@github.com>
2018-01-29 16:36:37 +0000

    Moved unneeded code into plot function

--- Overview_run.R
+++ Overview_run.R
@@ -12,13 +12,17 @@ plots_home<-paste(home,"plots",sep="")
 
 ##*** Code needed
 # Loads functions for generalised mean function and simulation model that uses the generalised function
-source("ec_generalised_function_withr.R") 
+source("ec_nosrdiff_generalised_function_withr.R") 
 
 ##*** Setting up
 # Number of discrete fitness levels? Resistance levels?
 nfit = 5;   mres = 5;
 # Array of distribution of fitness and resistance c[resistance, fitness]
 M0 <- array(0,c(mres,nfit,10))
+# M0 now needs initial condition - mostly "susceptible"
+wildtype <- c(1,5) # most fit least resistant
+susr <- wildtype[1]; susf <- wildtype[2]
+M0[susr,susf,1] <- 1
 
 # Initial acquisition distribution - bivariate here normal distribution with mean 0.5 and deviation 0.05
 x <- seq(1/mres,1,1/mres) # seq(from = 0, to = 1, length.out = mres)
@@ -27,26 +31,21 @@ f <- function(x, y) dmvnorm(cbind(x, y), mean = c(0.6, 0.6),sigma = diag(2)/20)
 z <- outer(x, y, FUN = f); z <- z/sum(z) # Generate and normalise
 dev.off(); persp(x, y, z, theta = -30, phi = 30, ticktype = "detailed")
 acqdistn<-z
-plot(rowSums(z),type="l");plot(colSums(z),type="l") # Same as normal distribution
-z<-as.data.frame(z)
-rownames(z) <- seq(1/mres,1,1/mres);colnames(z) <- seq(1,nfit,1);
-z2<-as.data.frame(melt(z)); z2$res<-seq(1/mres,1,1/mres); colnames(z2)<-c("fitness","value","res")
-p<-ggplot(z2, aes(x=res, y=value, fill=factor(fitness))) + geom_bar(stat="identity",colour="black") + facet_grid(~fitness) 
-p<-p + scale_x_continuous("Resistance level",breaks=c(0,0.2,0.4,0.6,0.8,1)) + scale_y_continuous("Proportion") + scale_fill_brewer("Fitness \nlevel",palette="Reds") + theme(axis.text.x = element_text(angle = 90, hjust = 1))
-p
-setwd(plots_home)
-ggsave("acqdistn_06.pdf",width=14,height=10)
 
 # Initial conditions
 iniv<-c(98,1) # Mostly space
 #iniv<-c(60,39,1)
+dt=0.1
+tsteps<-500*(1/dt)
+
 
 ####*** LOAD TO HERE ***######################################################################################################################################################################################################################################################################################################
 
 ####*** # (1) run with constant omega ***######################################################################################################################################################################################################################################################################################################
-
-
-
+omega_M <- c(24,16,2,0.4)
+submic_M <- c(1,1,1,1) # Linear decline
+pref <- "Constant_omega_"
+plot_diff_acd_output(acqdistn,"~/Documents/Hetero_res_and_f/plots",01, omega_M, submic_M, wildtype,pref)
 
 
 
